8a644df7282a988351b3484df1de0b6fa4bae7cb
sdevine188 <sdevine188@gmail.com>
2019-11-15 11:34:53 -0500

    updated

--- keras_scratchpad.R
+++ keras_scratchpad.R
@@ -168,10 +168,138 @@ df %>% mutate(row_number = row_number()) %>% filter(X260 != 0) %>% nrow()
 df %>% select(1, 260:265) %>% slice(1:10)
 
 
+##################################################################################################
+####################################################################################################
+####################################################################################################
 
 
+# regression
+# https://keras.rstudio.com/articles/tutorial_basic_regression.html
+
+# get data
+boston_housing <- dataset_boston_housing()
+boston_housing %>% glimpse()
+
+# split initial data to get vectors of train/test data/labels
+c(train_data, train_labels) %<-% boston_housing$train
+c(test_data, test_labels) %<-% boston_housing$test
+
+# inspect
+# note the train_data contains 13 variabels for each of 404 records, so its 5252 long
+paste0("Training entries: ", length(train_data), ", labels: ", length(train_labels))
+
+train_data[1, ] # Display sample features, notice the different scales
+train_labels[1:10] # note the labels are house prices in thousands
+
+# add column names
+column_names <- c('CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 
+                  'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT')
+train_df <- as_tibble(train_data)
+colnames(train_df) <- column_names
+train_df
 
 
+########################
 
 
+# normalize data
+# Test data is *not* used when calculating the mean and std.
 
+# Normalize training data
+train_data <- scale(train_data) 
+
+# Use means and standard deviations from training set to normalize test set
+col_means_train <- attr(train_data, "scaled:center") 
+col_stddevs_train <- attr(train_data, "scaled:scale")
+test_data <- scale(test_data, center = col_means_train, scale = col_stddevs_train)
+
+train_data[1, ] # First training sample, normalized
+
+
+##################
+
+
+# create the model
+build_model <- function() {
+        
+        model <- keras_model_sequential() %>%
+                layer_dense(units = 64, activation = "relu",
+                            input_shape = dim(train_data)[2]) %>%
+                layer_dense(units = 64, activation = "relu") %>%
+                layer_dense(units = 1)
+        
+        model %>% compile(
+                loss = "mse",
+                optimizer = optimizer_rmsprop(),
+                metrics = list("mean_absolute_error")
+        )
+        
+        model
+}
+
+model <- build_model()
+model %>% summary()
+
+
+#################
+
+
+# train the model
+# Display training progress by printing a single dot for each completed epoch.
+print_dot_callback <- callback_lambda(
+        on_epoch_end = function(epoch, logs) {
+                if (epoch %% 80 == 0) cat("\n")
+                cat(".")
+        }
+)    
+
+epochs <- 500
+
+# Fit the model and store training stats
+history <- model %>% fit(
+        train_data,
+        train_labels,
+        epochs = epochs,
+        validation_split = 0.2,
+        verbose = 0,
+        callbacks = list(print_dot_callback)
+)
+
+# inspect
+history
+
+library(ggplot2)
+plot(history, metrics = "mean_absolute_error", smooth = FALSE) +
+        coord_cartesian(ylim = c(0, 5))
+
+###############
+
+
+# update the fit to stop running new epochs once it stops improving 
+# The patience parameter is the amount of epochs to check for improvement.
+early_stop <- callback_early_stopping(monitor = "val_loss", patience = 20)
+
+model <- build_model()
+history <- model %>% fit(
+        train_data,
+        train_labels,
+        epochs = epochs,
+        validation_split = 0.2,
+        verbose = 0,
+        callbacks = list(early_stop, print_dot_callback)
+)
+
+plot(history, metrics = "mean_absolute_error", smooth = FALSE) +
+        coord_cartesian(xlim = c(0, 150), ylim = c(0, 5))
+
+
+###############
+
+
+# evaluate model on test set
+c(loss, mae) %<-% (model %>% evaluate(test_data, test_labels, verbose = 0))
+paste0("Mean absolute error on test set: $", sprintf("%.2f", mae * 1000))
+
+# predict on test set
+test_predictions <- model %>% predict(test_data)
+test_predictions[ , 1]
